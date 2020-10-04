28c02c97d44851c84589ec5053540e945b5ddb0f
Vaider7 <anton130101@mail.ru>
2020-10-04 23:36:53 +0500

    tssssss

--- public/js/index.js
+++ public/js/index.js
@@ -225,3 +225,127 @@ $(document).on('click', '.confirm_deleting_button', () => {
     })
 
 })
+
+$(document).on('click', '#gear', (e) => {
+    $('.task').each(function () {
+        if ($('.task').hasClass('changing_off')) {
+            return false
+        } else {
+            let $task = $(e.target).parents('.task')
+            $task.addClass('changing_off')
+            let taskHeader = $task.children('.first_line').children('.task_header').text()
+            let dateStart = $task.children('.second_line').children('.date').children('.date_start').text()
+            let dateEnd = $task.children('.second_line').children('.date').children('.date_end').text()
+            let priority = $task.children('.second_line').children('.priority').children('.priority_value').text()
+            let status = $task.children('.third_line').children('.status').children('.status_value').text()
+            let updated = $task.children('.third_line').children('.update').children('.update_value').text()
+            let description = $task.children('.description_container').children('.description').text()
+
+            let idTask = $task.attr('id')
+
+            let priorityHigh, priorityMedium, priorityLow
+
+            priorityHigh = ''
+            priorityMedium = ''
+            priorityLow = ''
+
+            if (priority === 'Высокий') {
+                priorityHigh = 'selected'
+            }
+
+            if (priority === 'Средний') {
+                priorityMedium = 'selected'
+            }
+
+            if (priority === 'Низкий') {
+                priorityLow = 'selected'
+            }
+
+            let statusCanceling, statusDone, statusInProcess
+
+            statusCanceling = ''
+            statusDone = ''
+            statusInProcess = ''
+
+            if (status === 'К выполнению') {
+                statusInProcess = 'selected'
+            }
+
+            if (status === 'Выполнена') {
+                statusDone = 'selected'
+            }
+
+            if (status === 'Отмена') {
+                statusCanceling = 'selected'
+            }
+
+            $task.addClass('hidden')
+
+
+            $task.after(`<div class="task changing_on" id="${idTask}">\n` +
+                '    <form class="new_task">\n' +
+                '        <div class="first_line">\n' +
+                `            <div class="task_header"><input type="text" name="task_header" class="task_header_input" placeholder="Заголовок" value="${taskHeader}"></div>\n` +
+                '        </div>\n' +
+                '        <div class="second_line">\n' +
+                '            <div class="date">\n' +
+                '                <div class="timing">Сроки:&nbsp;</div>\n' +
+                `                <div class="date_start"><input type="text" name="date_start" class="date_input date_start_input" value="${dateStart}">\n` +
+                '                </div><div class="dash">-&nbsp;</div>\n' +
+                `                <div class="date_end"><input type="text" name="date_end" class="date_input date_end_input" value="${dateEnd}"></div>\n` +
+                '                <div class="days_left"><span class="days_left_value"></span></div>\n' +
+                '            </div>\n' +
+                '            <div class="priority">Приоритет:&nbsp;\n' +
+                '                <span class="priority_value">\n' +
+                '                        <select name="priority" class="priority_select">\n' +
+                `                            <option value="high" ${priorityHigh}>Высокий</option>\n` +
+                `                            <option value="medium" ${priorityMedium}>Средний</option>\n` +
+                `                            <option value="low" ${priorityLow}>Низкий</option>\n`+
+                '                        </select>\n' +
+                '                    </span>\n' +
+                '            </div>\n' +
+                '        </div>\n' +
+                '        <div class="third_line">\n' +
+                '            <div class="status">Статус:&nbsp;\n' +
+                '                <span class="status_value">\n' +
+                '                        <select name="status" class="status_select">\n' +
+                `                            <option value="in_process" ${statusInProcess}>К выполнению</option>\n` +
+                `                            <option value="done" ${statusDone}>Выполнена</option>\n` +
+                `                            <option value="canceling" ${statusCanceling}>Отмена</option>\n` +
+                '                        </select>\n' +
+                '                    </span>\n' +
+                '            </div>\n' +
+                `            <div class="update">Дата обновления:&nbsp;<span class="update_value"><input type="text" name="updated" class="date_input update_input" value="${updated}"></span></div>\n` +
+                '        </div>\n' +
+                '        <div class="description_line">\n' +
+                '            <div class="description_header">Описание:</div>\n' +
+                '        </div>\n' +
+                `        <textarea class="textarea" accesskey name="description">${description}</textarea>\n` +
+                '        <button type="button" class="submit cancel_changing_button">Отменить</button>\n' +
+                '        <button type="button" class="submit confirm_changing_button">Сохранить</button>\n' +
+                '        <div class="wrong_message"></div>' +
+                '    </form>\n' +
+                '</div>')
+            $('.date_input').each(function () {
+                new Cleave(this, {
+                    date: true,
+                    delimiter: '.',
+                    datePatter: ['d', 'm', 'Y']
+                })
+            })
+        }
+    })
+})
+
+$(document).on('click', '.cancel_changing_button', () => {
+    $('.task').each(function () {
+        if ($(this).hasClass('changing_off')) {
+            $(this).removeClass('changing_off')
+            $(this).removeClass('hidden')
+        }
+        if ($(this).hasClass('changing_on')) {
+            $(this).remove()
+            return false
+        }
+    })
+})
