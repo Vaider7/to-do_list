46a0cbb2f478227d63460f179bbef29d5a92fbac
Vaider7 <anton130101@mail.ru>
2020-10-16 12:20:54 +0500

    122

--- config/config.js
+++ config/config.js
@@ -1,12 +1,12 @@
 module.exports = {
     PORT: 3000,
     MYSQL_CONFIG: {
-        host: "127.0.0.1",
-        user: "test",
-        database: "to-do_list",
-        password: "Qwerty12345",
+        host: "sql7.freemysqlhosting.net",
+        user: "sql7370959",
+        database: "sql7370959",
+        password: "J5kiHvmAee",
         port: "3306"
-},
+    },
     SECRET_CODE_SESSION: '5N2h3a4%vwL&xMN8sVBL6N$sT',
     STORE_OPTIONS: {
         clearExpired: true,
